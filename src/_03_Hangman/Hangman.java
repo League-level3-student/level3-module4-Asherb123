2fd4bf54d2585eb6a9e8e5feea50d1383508692e
Asherb123 <46113623+Asherb123@users.noreply.github.com>
2022-04-27 18:26:56 -0700

    work on hangman

--- src/_03_Hangman/Hangman.java
+++ src/_03_Hangman/Hangman.java
@@ -1,5 +1,28 @@
 package _03_Hangman;
 
+import javax.swing.JOptionPane;
+
 public class Hangman {
+public static void main(String[] args) {
+	String s = JOptionPane.showInputDialog("Type in any random number between 1 and 100");
+	
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
