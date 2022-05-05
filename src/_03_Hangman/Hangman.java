989a0854cda01ddaec36a1793988b8544cb5172b
Asherb123 <46113623+Asherb123@users.noreply.github.com>
2022-05-04 18:29:19 -0700

    Finish hangman

--- src/_03_Hangman/Hangman.java
+++ src/_03_Hangman/Hangman.java
@@ -1,11 +1,24 @@
 package _03_Hangman;
 
+import java.util.Stack;
+
 import javax.swing.JOptionPane;
 
 public class Hangman {
 public static void main(String[] args) {
-	String s = JOptionPane.showInputDialog("Type in any random number between 1 and 100");
+	String randNum = JOptionPane.showInputDialog("Type any random number between 1 and 100");
 	
+	int random = Integer.parseInt(randNum);
+	
+	Stack <String> words = new Stack<>();
+	
+	for (int i = 0; i < random; i++) {
+	
+	words.push(Utilities.readRandomLineFromFile("dictionary.txt"));
+		
+		
+		
+	}
 	}
 	
 	
